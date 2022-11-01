f60d3ee4285b73d432dbe8aaad7c95726c48f7f8
yarmushn@gmail.com <yarmushn@gmail.com>
2022-11-01 20:27:11 +0300

    change port 8

--- public/js/script.js
+++ public/js/script.js
@@ -2,7 +2,7 @@ const socket = io('/')
 const myPeer = new Peer(undefined, {
     key: "peerjs",
     debug: 2,
-    secure: process.env.REACT_APP_ENV === "PRODUCTION" ? true : false,
+    // secure: process.env.REACT_APP_ENV === "PRODUCTION" ? true : false,
 })
 let peers = {}
 
