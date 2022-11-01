cdd8decc1f039ec8ddc908ea7e58d7b7a10894fd
yarmushn@gmail.com <yarmushn@gmail.com>
2022-11-01 19:50:11 +0300

    change port

--- public/js/script.js
+++ public/js/script.js
@@ -1,7 +1,7 @@
 const socket = io('/')
 const myPeer = new Peer(undefined, {
     secure: true, 
-    host: 'https://cassette-player.herokuapp.com', 
+    host: 'cassette-player.herokuapp.com', 
     port: 9000,
 })
 let peers = {}
